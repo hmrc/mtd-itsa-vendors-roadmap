0065467248f97c12dfd124a38d313a30f91ce920
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2021-08-26 09:50:20 +0100

    bump sbt dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,9 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "5.7.0"
-
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.12.0")
 
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" %  "flexmark-all" % "0.36.8" % "test",
