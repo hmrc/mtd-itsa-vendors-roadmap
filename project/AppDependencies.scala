4ea8c8cf4f17abc3bd0427963a8d385ad1e51c0d
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2022-05-09 16:15:31 +0100

    [MTDSA-11773] Updated dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,10 +4,10 @@ import sbt._
 object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.21.0")
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0")
 
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" %  "flexmark-all" % "0.62.2" % "test",
-    "org.scalatest"        %% "scalatest"    % "3.2.11" % "test"
+    "org.scalatest"        %% "scalatest"    % "3.2.12" % "test"
   )
 }
