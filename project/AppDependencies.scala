f1623f3d991d94dfd1377988d104407acb0eae0f
tom-lloyd-hmrc <112491110+tom-lloyd-hmrc@users.noreply.github.com>
2023-06-09 09:17:25 +0100

    MTDSA-16569: Upgrade SBT Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,16 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
+
+  val bootStrapPlayVersion = "7.11.0"
+
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0")
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
 
   val test: Seq[ModuleID] = Seq(
-    "com.vladsch.flexmark" %  "flexmark-all" % "0.62.2" % "test",
-    "org.scalatest"        %% "scalatest"    % "3.2.12" % "test"
+    "com.vladsch.flexmark" %  "flexmark-all"          % "0.64.6"             % "test",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test",
+    "org.scalatest"       %% "scalatest"              % "3.2.15"             % "test"
   )
 }
