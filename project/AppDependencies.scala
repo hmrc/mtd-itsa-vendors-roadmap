a6798e57eb47c6f1e08c80ca782b3be85e53c90f
Gaurav <41486966+GauravJai@users.noreply.github.com>
2024-02-26 11:49:05 +0000

    MTDSA-21261: Updated play version to 3.0 + crown (#87)
    * MTDSA-21261: Updated play version to 3.0 + crown
    
    * update plugin versions
    
    * config update
    
    * updated gemfile.lock platforms to ruby

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapPlayVersion = "7.15.0"
+  val bootStrapPlayVersion = "8.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapPlayVersion)
 
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" %  "flexmark-all"          % "0.64.6"             % "test",
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootStrapPlayVersion % "test",
     "org.scalatest"       %% "scalatest"              % "3.2.15"             % "test"
   )
 }
