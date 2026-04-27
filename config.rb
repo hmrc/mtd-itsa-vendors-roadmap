3c124bb91bc38f9cd6c755f9cda38d5ce28a6721
muhammadmulla1 <muhammad_mulla@icloud.com>
2026-04-27 15:51:30 +0100

    MTDSA-33950: changes

--- config.rb
+++ config.rb
@@ -4,3 +4,9 @@ GovukTechDocs.configure(self)
 
 set :relative_links, true
 activate :relative_assets
+
+activate :content_security_policy do |policy|
+  policy.script_src :self,
+                    "http://localhost:12345",
+                    "/tracking-consent"
+end
