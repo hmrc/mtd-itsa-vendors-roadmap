5be8b93ff9bbeba3209a55d49760fcbcc893c718
muhammadmulla1 <muhammad_mulla@icloud.com>
2026-04-27 16:22:45 +0100

    MTDSA-33950: Consolidate documentation files and configure local tracking in Income Tax (MTD) roadmap

--- config.rb
+++ config.rb
@@ -4,9 +4,3 @@ GovukTechDocs.configure(self)
 
 set :relative_links, true
 activate :relative_assets
-
-activate :content_security_policy do |policy|
-  policy.script_src :self,
-                    "http://localhost:12345",
-                    "/tracking-consent"
-end
